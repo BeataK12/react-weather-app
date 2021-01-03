3b6147234870ab1f27b717fb229e8b4942530cbb
Beata <lbeata92@yahoo.com>
2021-01-03 23:40:55 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
      
     </div>
       <small>
-      <a href="https://github.com/BeataK12/react-weather-app" target="_blank" rel="gitlink">
+      <a href="https://github.com/BeataK12/react-weather-app" target="_blank" rel="noreferrer">
         Open-source code</a
       >, by Beata Kolbai</small
     >
